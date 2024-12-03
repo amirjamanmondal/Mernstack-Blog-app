c811849240591d73eeda7ef26d6bbd9419ca94e1
Amir Jaman Mondal <amirjamanmondal@gmail.com>
2024-12-03 22:47:57 +0530

    Update AuthUser.js

--- backend/middleware/AuthUser.js
+++ backend/middleware/AuthUser.js
@@ -1,6 +1,3 @@
-const jwt = require("jsonwebtoken");
-
-// Middleware to check if user is authenticated with error handling
 function isAuthenticated(req, res, next) {
   try {
     if (req.isAuthenticated()) {
@@ -9,7 +6,7 @@ function isAuthenticated(req, res, next) {
     res.status(401).json({ message: "user Not Logged in" });
   } catch (error) {
     console.error("Authentication error:", error);
-    res.status(500).send("Internal Server Error"); // Handle any unexpected errors
+    res.status(500).send("Internal Server Error");
   }
 }
 
